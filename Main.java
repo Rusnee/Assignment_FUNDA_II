8e2534fd67c6e909eb5dc2df16f932aed5fc46a4
Rusnee <kat.rusnee.buena@gmail.com>
2016-04-15 01:13:30 +0700

    #01 สร้างหน้าแสดงผลเกมส์ (windows) ขึ้นมาได้
    สามารถทำความเข้าใจ การสร้าง Frame
    และสามารถสร้างแสดงหน้าต่างเกมส์ขึ้นมาได้

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args){
-		JFrame frame = new JFrame("My App");
+		JFrame frame = new JFrame("My GAME FUNDA II");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		
