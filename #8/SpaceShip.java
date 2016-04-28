35f6f7a71f23812aae83b61b6b759237b8e773c4
Rusnee <kat.rusnee.buena@gmail.com>
2016-04-28 21:29:53 +0700

    #07 วัตถุตกลงมา เมื่อชนยานของเราจะจบเกมส์
    วัตถุตกลงมา เมื่อชนยานของเราจะจบเกมส์

---
+++ #8/SpaceShip.java
@@ -0,0 +1,36 @@
+//package f2.spw;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class SpaceShip extends Sprite{
+
+	int step = 10;
+	
+	public SpaceShip(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(x, y, width, height);
+		
+	}
+
+	public void moveX(int direction){
+		x += (step * direction);
+		if(x < 0)
+			x = 0;
+		if(x > 400 - width)
+			x = 400 - width;
+	}
+	public void moveY(int direction){
+		y += (step * direction);
+		if(y < 0)
+			y = 0;
+		if(y > 600 - height)
+			y = 600 - height;
+	}
+}
